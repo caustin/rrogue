1a2b32fd068444cf54702a00c2665f10e6d1a24d
chris <127845+caustin@users.noreply.github.com>
2025-07-20 18:39:17 -0500

    Final pre-messaging refactor.  lots of coupling still but was able to compromise by using the game package for a ton of stuff.

---
+++ game/game.go
@@ -0,0 +1,72 @@
+package game
+
+import (
+	"github.com/bytearena/ecs"
+	"github.com/caustin/rrogue/config"
+	"github.com/hajimehoshi/ebiten/v2"
+)
+
+// Game holds all data the entire game will need.
+type Game struct {
+	Map           GameMap
+	World         *ecs.Manager
+	WorldTags     map[string]ecs.Tag
+	Components    *ComponentReferences
+	GameData      config.GameData
+	Turn          TurnState
+	TurnCounter   int
+	AutoMoveState *AutoMoveState
+}
+
+// NewGame creates a new Game Object and initializes the data
+// This is a pretty solid refactor candidate for later
+func NewGame() *Game {
+	g := &Game{}
+	g.Map = NewGameMap()
+	g.GameData = config.NewGameData()
+	world, tags, components := InitializeWorld(g.Map.CurrentLevel)
+
+	g.WorldTags = tags
+	g.World = world
+	g.Components = components
+	g.Turn = WaitingForPlayerInput
+	g.TurnCounter = 0
+	return g
+
+}
+
+// Update is called each tic.
+func (g *Game) Update() error {
+	switch g.Turn {
+	case WaitingForPlayerInput:
+		if TakePlayerAction(g) {
+			g.Turn = ProcessingMonsterTurn
+			g.TurnCounter++
+		}
+	case ProcessingMonsterTurn:
+		UpdateMonster(g)
+		g.Turn = WaitingForPlayerInput
+	default:
+		panic("unhandled default case")
+	}
+
+	return nil
+
+}
+
+// Draw is called each draw cycle and is where we will blit.
+func (g *Game) Draw(screen *ebiten.Image) {
+	//Draw the Map
+	level := g.Map.CurrentLevel
+	level.DrawLevel(screen, g.GameData)
+	ProcessRenderables(g, level, screen)
+	ProcessUserLog(g, screen)
+	ProcessHUD(g, screen)
+
+}
+
+// Layout will return the screen dimensions.
+func (g *Game) Layout(w, h int) (int, int) {
+	return g.GameData.TileWidth * g.GameData.ScreenWidth, g.GameData.TileHeight * g.GameData.ScreenHeight
+
+}
--- main.go
+++ main.go
@@ -1,83 +1,16 @@
 package main
 
 import (
-	"github.com/caustin/rrogue/config"
 	"github.com/caustin/rrogue/game"
 	_ "image/png"
 	"log"
 
-	"github.com/bytearena/ecs"
 	"github.com/hajimehoshi/ebiten/v2"
 )
 
-// Game holds all data the entire game will need.
-type Game struct {
-	Map           game.GameMap
-	World         *ecs.Manager
-	WorldTags     map[string]ecs.Tag
-	Components    *game.ComponentReferences
-	GameData      config.GameData
-	Turn          game.TurnState
-	TurnCounter   int
-	AutoMoveState *AutoMoveState
-}
-
-// NewGame creates a new Game Object and initializes the data
-// This is a pretty solid refactor candidate for later
-func NewGame() *Game {
-	g := &Game{}
-	g.Map = game.NewGameMap()
-	g.GameData = config.NewGameData()
-	world, tags, components := game.InitializeWorld(g.Map.CurrentLevel)
-
-	g.WorldTags = tags
-	g.World = world
-	g.Components = components
-	g.Turn = game.WaitingForPlayerInput
-	g.TurnCounter = 0
-	return g
-
-}
-
-// Update is called each tic.
-func (g *Game) Update() error {
-	switch g.Turn {
-	case game.WaitingForPlayerInput:
-		if TakePlayerAction(g) {
-			g.Turn = game.ProcessingMonsterTurn
-			g.TurnCounter++
-		}
-	case game.ProcessingMonsterTurn:
-		UpdateMonster(g)
-		g.Turn = game.WaitingForPlayerInput
-	default:
-		panic("unhandled default case")
-	}
-
-	return nil
-
-}
-
-// Draw is called each draw cycle and is where we will blit.
-func (g *Game) Draw(screen *ebiten.Image) {
-	//Draw the Map
-	level := g.Map.CurrentLevel
-	level.DrawLevel(screen, g.GameData)
-	ProcessRenderables(g, level, screen)
-	ProcessUserLog(g, screen)
-	ProcessHUD(g, screen)
-
-}
-
-// Layout will return the screen dimensions.
-func (g *Game) Layout(w, h int) (int, int) {
-	return g.GameData.TileWidth * g.GameData.ScreenWidth, g.GameData.TileHeight * g.GameData.ScreenHeight
-
-}
-
 func main() {
 
-	g := NewGame()
+	g := game.NewGame()
 	ebiten.SetWindowResizable(true)
 
 	ebiten.SetWindowTitle("Tower")
